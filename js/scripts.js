e9fab552634c057f71c7d436ac74a9f17677c0e4
MatiSami <mateusz.samula@gmail.com>
2020-04-05 21:41:59 +0200

    dodanie RWD

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+const menu = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector(".main-nav-mobile")
+const scrollBtn = document.querySelector('.scroll-btn')
+
+menu.addEventListener('click', () => { 
+    menu.classList.toggle('hamburger--active'); 
+
+    if (menu.classList.contains("hamburger--active")){
+        mobileMenu.classList.add("main-nav-mobile--active")
+    } else {
+        mobileMenu.classList.remove("main-nav-mobile--active")
+    }
+});   
+
+mobileMenu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("main-nav-mobile--active")
+    menu.classList.remove('hamburger--active'); 
+})
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 20) {
+      scrollBtn.style.display = "block";
+    } else {
+      scrollBtn.style.display = "none";
+    }
+  }
+
+
+  scrollBtn.addEventListener("click", () => {
+document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  })
+  
+    
+ 
